13ac2f9df6ec165ffd8ddf10e3decdb6232003e5
Noah Broyles <noahbroyles@bestnest.com>
2022-07-15 14:57:05 -0400

    Simplify regexes

--- secsie/__init__.py
+++ secsie/__init__.py
@@ -25,16 +25,16 @@ from pathlib import Path
 
 MODES = {
     "secsie": dict(
-        SECTION_EX=re.compile(r'^\[([a-zA-Z0-9_-]+)\]$'),
-        FLOAT_EX=re.compile(r'^(-?\d+[\.]\d*)$'),
+        SECTION_EX=re.compile(r'^\[([a-zA-Z\d_-]+)]$'),
+        FLOAT_EX=re.compile(r'^(-?\d+[.]\d*)$'),
         FALSE_EX=re.compile(r'^(false|no)$', re.IGNORECASE),
         NULL_EX=re.compile(r'^null$', re.IGNORECASE),
         TRUE_EX=re.compile(r'^(true|yes)$', re.IGNORECASE),
         INT_EX=re.compile(r'^-?\d+$')
     ),
     "ini": dict(
-        SECTION_EX=re.compile(r'^\[([a-zA-Z0-9 _-]+)\]$'),
-        FLOAT_EX=re.compile(r'^(-?\d+[\.]\d*)$'),
+        SECTION_EX=re.compile(r'^\[([a-zA-Z\d _-]+)]$'),
+        FLOAT_EX=re.compile(r'^(-?\d+[.]\d*)$'),
         FALSE_EX=re.compile(r'^(false|no)$', re.IGNORECASE),
         NULL_EX=re.compile(r'^null$', re.IGNORECASE),
         TRUE_EX=re.compile(r'^(true|yes)$', re.IGNORECASE),

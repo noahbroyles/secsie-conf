edc6201f87970601c476b5ca1a2f35cdd7eda8ff
Noah Broyles <noah@x3nzpouwu.com>
2022-12-22 13:49:14 -0500

    Add useful comments

--- releaseUpdate.py
+++ releaseUpdate.py
@@ -2,7 +2,11 @@
 
 import subprocess
 
-
+# Remove the old build directories, if any
 subprocess.call(['rm', '-fr', 'build/', 'dist/'])
+
+# Run setup.py
 subprocess.call(['python3', 'setup.py', 'sdist', 'bdist_wheel'])
+
+# Use Twine to upload the output to PyPi
 subprocess.call(['twine', 'upload', 'dist/*'])

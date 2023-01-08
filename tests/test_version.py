8589798837b767d2a606498c8c3a927a936941c6
Noah Broyles <noah@javamate.net>
2023-01-08 17:46:18 -0500

    Add extra documentation

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,6 +3,9 @@ import secsie
 
 
 def test_secsie_semver_syntax():
+    """
+    Ensure that the current version of secsie is appropriate according to the semantic versioning ideology.
+    """
     VERSION_EX = re.compile(
         r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
     )

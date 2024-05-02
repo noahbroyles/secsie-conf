6aca4f12f780965a1eaa40aa9ac71b9955e12319
Noah Broyles <noah@x3nzpouwu.com>
2024-05-02 13:43:19 -0400

    Update compatible Python versions

--- setup.py
+++ setup.py
@@ -24,7 +24,9 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10"
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
     ],
     packages=["secsie"],
     include_package_data=True

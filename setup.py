ad440cf8251606eba7423ee1a9ae5cdfeba18f34
Noah Broyles <noah@x3nzpouwu.com>
2026-02-09 18:04:17 -0500

    Add support for 3.13 and 3.14

--- setup.py
+++ setup.py
@@ -26,7 +26,9 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12"
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     packages=["secsie"],
     include_package_data=True
